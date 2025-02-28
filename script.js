08422cba42123f03f780cc7639fe16136472e25b
Cattuong-03 <cattuong.dm@gmail.com>
2025-02-28 10:19:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function login()
+{
+  let u = username.value
+  let p = pass.value
+  if (u=="Catu" && p=="Catu2103")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
